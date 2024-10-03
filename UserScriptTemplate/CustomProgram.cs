56baeb24800c6ebb656e2f0311b71d81f44172ab
k-edr <yevhen.puzenko@gmail.com>
2024-10-03 12:09:27 +0200

    added Environment & Logging to template

--- UserScriptTemplate/CustomProgram.cs
+++ UserScriptTemplate/CustomProgram.cs
@@ -1,4 +1,6 @@
-﻿using Sandbox.ModAPI.Ingame;
+﻿using IngameScript.Pulse.EnvironmentVariables;
+using IngameScript.Pulse.Logging;
+using Sandbox.ModAPI.Ingame;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,8 +11,19 @@ namespace IngameScript
 {
     public partial class Program : MyGridProgram
     {
+        protected IMyIni Environment;
+
+        protected ILoggable Logger;
+
         public Program()
         {
+            Environment = new MyIniConfigParser(Me);
+
+            Logger = new FlushLogger(
+                new PanelLogger(
+                    GridTerminalSystem.GetBlockWithName(
+                        Environment.Get("Grid", "LoggerPanel").ToString()) as IMyTextPanel));
+
             Init();
         }
 
